47aaa6fa1bacf56c03b6f41a1ce8b6370fe42cca
Teal Emery <ltemery@gmail.com>
2023-11-29 13:06:10 -0500

    Added to description & raw data
    Added package description.  Began processing raw data

---
+++ data-raw/DATASET.R
@@ -0,0 +1,10 @@
+## code to prepare `DATASET` dataset goes here
+
+# File folder downloaded and unzipped from  https://docs.aiddata.org/ad4/datasets/AidDatas_Global_Chinese_Development_Finance_Dataset_Version_3_0.zip
+
+# define file path
+file_path <- here::here("data-raw",
+           "AidDatas_Global_Chinese_Development_Finance_Dataset_Version_3_0",
+           "AidDatasGlobalChineseDevelopmentFinanceDataset_v3.0.xlsx")
+
+#usethis::use_data(DATASET, overwrite = TRUE)
